97a5235b6176a94cdac35c7ee7ae2cc4b45c5210
Hamza <dzcyberdev@gmail.com>
2021-09-01 09:39:26 +0200

    init analysis Android x64

--- boring_flutter.py
+++ boring_flutter.py
@@ -5,6 +5,7 @@ import os
 
 
 start_time = time.time()
+search_scalar = '0x186'  # TODO: currently HARD CODED, add argument parameter
 
 if len(sys.argv) < 2:
     print('Usage: python {} libflutter.so'.format(sys.argv[0]))
@@ -33,4 +34,38 @@ if info['bin']['bits'] != 64:
     print('Currently only supporting x64...')
     exit(0)
 
+print('🔥 Performing Advanced analysis (aaaa)...')
+r.cmd('aaaa')
+
+print('🔥 Searching for instructions with scalar value (/ae {},)...'.format(search_scalar))
+search = r.cmd('/ae {},'.format(search_scalar))
+
+target = ''
+for hit in search.splitlines():
+    if hit.startswith('0x005') and 'mov' in hit:
+        target = hit.split(' ')[0]
+        print('\033[31m{}\033[0m'.format(hit))
+    else:
+        print(hit)
+
+if not target:
+    print('Could not find a mov instruction with {} scalar value, in 0x005 region...'.format(search_scalar))
+    exit(0)
+else:
+    print('Found "{}", a mov instruction with {} scalar value, in 0x005 region...'.format(target, search_scalar))
+
+print('🔥 Seeking to target (s {})...'.format(target))
+r.cmd('s {}'.format(target))
+
+fcn_addr = r.cmd('afi.')
+address = '0x' + fcn_addr.split('.')[1].strip()
+
+print('🔥 Found ssl_crypto_x509_session_verify_cert_chain @ {} (afi.)...'.format(address))
+
+
+with open('template_frida_hook.js') as f:
+    template = f.read()
+
 print('🔥 Happy MITM!!! ({}s)'.format(time.time() - start_time))
+print('-' * 50)
+print(template.replace('0x00000000', address))
